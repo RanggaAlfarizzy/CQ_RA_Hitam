781febca3c59a1395cc540b6a1a7bc0d473263a8
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2026-03-12 15:24:04 +0800

    Menambahkan fitur koneksi database

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -31,5 +31,22 @@ namespace PraktikumADO
         {
 
         }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                MessageBox.Show("Koneksi ke database berhasil!");
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
