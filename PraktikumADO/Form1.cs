a5a0b4a9dfb61954f524f3b34972a6cc452846db
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2026-03-12 19:46:08 +0800

    Menambahkan Kerangka Untuk nanti memasukkan fitur Insert Data Ke program Studi

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -165,5 +165,10 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
+
+        private void btnInsertProdi_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
