6a2c753a2cf8c1dedbd9567a6e11f0f3a8314192
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2026-03-12 17:20:26 +0800

    Menambah Kerangka button Hitung jumlah Dosen

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -117,5 +117,10 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
+
+        private void btnHitungDosen_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
