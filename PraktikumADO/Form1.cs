e6df7590284d13046a343ac9e51df1dd557de488
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2026-03-12 15:13:58 +0800

    Add project files.

---
+++ PraktikumADO/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PraktikumADO
+{
+    public partial class Form1: Form
+    {
+        SqlConnection conn;
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Koneksi()
+        {
+            conn = new SqlConnection(
+                "Data Source=LAPTOP-9IBDVE0G\\RANGGAALFARIZZY;Initial Catalog=DBAkademikADO;Integrated Security=True"
+                );
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
